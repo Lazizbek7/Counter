ac8db13b5773f60d0b598755bb6fae227f34789b
Lazizbek <lazizbek19831910@gmail.com>
2024-01-29 17:53:41 +0500

    add all counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Test from "./components/script";
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Test/>
+      <div className="Wrap">
+    <div className="Counter">
+      <label id="count">{counter}</label>
+      <button id="minus_Btn" onClick={() => setCounter(counter - 1)}>
+        -
+      </button>
+      <button id="reset_Btn" onClick={() => setCounter(0)}>
+        reset
+      </button>
+      <button id="Plus_Btn" onClick={() => setCounter(counter + 1)}>
+        +
+      </button>
+    </div></div>
+    </>
   );
 }
 
